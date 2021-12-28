4aca632c73e3124e594b0a8c6115a84f87c0cfe2
Majharul Islam <majharul_nayem@yahoo.com>
2021-12-28 19:25:07 +0600

    custom error handler added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +37,14 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('v1/*')) {
+                return response()->json([
+                    'error' => 'Data Not Found!'
+                ], 404);
+            }
         });
     }
 }
