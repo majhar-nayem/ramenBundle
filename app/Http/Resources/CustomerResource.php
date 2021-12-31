cb2de34775931266fabb2e04f68728b01a2d7271
Majharul Islam <majharul_nayem@yahoo.com>
2021-12-31 20:47:42 +0600

    customer resoruce updated

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -14,6 +14,14 @@ class CustomerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'image' => $this->image ? asset($this->image) : null,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
